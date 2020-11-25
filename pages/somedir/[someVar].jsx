3359e3ade7b7e7768ab7011337d9404761f02d59
Lindsay Levine <lindslev.ll@gmail.com>
2020-11-24 22:43:54 -0500

    add ssr'd page with netlify toml

---
+++ pages/somedir/[someVar].jsx
@@ -0,0 +1,17 @@
+export default function SomeVar({ someVar })  {
+  return (
+    <html>
+      <body>
+        Variable: {someVar}
+      </body>
+    </html>
+  )
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      someVar: context.params.someVar
+    }
+  }
+}
